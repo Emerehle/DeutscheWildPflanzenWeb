cb73a779091a49c53af26d015fe18884c99587d8
Emerehle <140219471+Emerehle@users.noreply.github.com>
2025-05-29 22:34:34 +0200

    build: postgresql conection

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,6 +1,7 @@
 import express, {Application, Request, Response} from 'express';
 
 const app: Application = express();
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hallo, Welt!');
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,6 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import app from './app';
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Server läuft auf Port ${port}`);
+  console.log(`Server läuft auf Port ${port}`);
 });
